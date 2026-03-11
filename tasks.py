478d0f4afc4df8bbe2dc634630164ebb47fa3c7d
David <pppa8691@gmail.com>
2026-03-11 09:30:41 +0100

    Added more examples in tasks

--- tasks.py
+++ tasks.py
@@ -1,5 +1,12 @@
 tasks = [
     {"task": "Preparar presentación", "deadline_days": 2, "duration": 3},
     {"task": "Estudiar Python", "deadline_days": 7, "duration": 2},
-    {"task": "Ir al gimnasio", "deadline_days": 5, "duration": 1}
+    {"task": "Ir al gimnasio", "deadline_days": 5, "duration": 1},
+    {"task": "Pasear al perro", "deadline_days": 1, "duration": 1},
+    {"task": "Hacer la compra", "deadline_days": 3, "duration": 1},
+    {"task": "Limpiar la habitación", "deadline_days": 4, "duration": 2},
+    {"task": "Leer un capítulo de un libro", "deadline_days": 6, "duration": 1},
+    {"task": "Llamar a un amigo", "deadline_days": 5, "duration": 1},
+    {"task": "Organizar el escritorio", "deadline_days": 4, "duration": 1},
+    {"task": "Revisar correos", "deadline_days": 2, "duration": 1}
 ]
